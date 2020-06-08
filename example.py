59edd3178d9bfd53a1d4070f153d84336db91e2c
Dang Pham <dcp237@cornell.edu>
2020-06-08 18:18:24 -0400

    initial commit

---
+++ example.py
@@ -0,0 +1,38 @@
+from anothertimer import Timer
+import matplotlib.pyplot as plt
+
+def fib(n:int) -> int:
+    assert n > 0 
+    if n==1: 
+        return 0
+    elif n==2: 
+        return 1
+    else: 
+        return fib(n-1)+fib(n-2)         
+
+# set up the timer        
+timer = Timer()
+
+# time the Fibonacci run
+for i in range(33):
+    timer.tic()
+    print(i,fib(i+1))
+    timer.toc()
+
+# save the time
+timer.dump(filename='example.csv',mode='w')
+
+# load all runs to plot
+timer.load('example.csv')
+fig, ax = timer.plot(plot_start_time=True, plot_avg=False)
+plt.savefig('example.pdf')
+plt.show()
+
+# set up another Timer object to read a file
+timer2 = Timer()
+
+# load the example run
+timer.load('example2.csv')
+fig, ax = timer.plot(plot_start_time=True, plot_avg=True)
+plt.savefig('example2.pdf')
+plt.show()
